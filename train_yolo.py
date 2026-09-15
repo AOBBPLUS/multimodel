91347374c2c838edc0b1bc1b5158c32fa6005e83
aobaba <2107406953@qq.com>
2026-09-15 16:18:05 +0800

    feat: 识别模型微调，语言文字互转

---
+++ train_yolo.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+
+if __name__ == "__main__":
+    model = YOLO("yolo11n.pt")
+    results = model.train(
+        data="datasets/defect.yaml",
+        epochs=5,
+        imgsz=640,
+        batch=8,
+        lr0=0.001,
+        name="defect_v5"
+    )
+
+    # 使用微调后的模型进行预测
+    # model = YOLO("runs/detect/defect_v5-2/weights/best.pt")
+    # results = model.predict(source="static/test.jpg", conf=0.1)
+    # print(results[0].boxes.cls)
